2de59e0eb495eb30a1dfbc41f69bbbaae1632a45
tdimnet <thomas.dimnet@gmail.com>
2020-07-27 09:12:08 +0200

    Create example structure for the librairy.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import TextInput from './lib/TextInput'
+
+const App = () => (
+  <div style={{ width: 640, margin: "15px auto" }}>
+    <h1>Hello world</h1>
+    <TextInput label="Email Address" placeholder="name@example.com" />
+  </div>
+)
+
 ReactDOM.render(
-  <div>Hello world</div>,
+  <App />,
   document.getElementById('root')
 );
